073c83f4b387f6f6ab3babeb71d0c7a717bfed54
arpit107 <arpit.29082011@gmail.com>
2016-07-13 14:51:09 +0530

    removed demo details

--- src/main/java/com/pfy/repository/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.pfy.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pfy.domain.entity.EmployeeDetailss;
-
-public interface EmployeeRepo extends JpaRepository<EmployeeDetailss, Integer> {
-
-}
