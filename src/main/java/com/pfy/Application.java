35e3dffc5ee579080e8af0a42c20d62db533f9c7
arpit107 <arpit.29082011@gmail.com>
2016-07-13 10:41:58 +0530

    session

--- src/main/java/com/pfy/Application.java
+++ src/main/java/com/pfy/Application.java
@@ -1,7 +1,10 @@
 package com.pfy;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.jpa.HibernateEntityManagerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -10,6 +13,12 @@ public class Application {
 
     public static void main(String[] args) throws Throwable {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    public SessionFactory sessionFactory(HibernateEntityManagerFactory hemf)
+    {
+    	return hemf.getSessionFactory();
     }
 
 }
--- src/main/java/com/pfy/GreetingController.java
+++ src/main/java/com/pfy/GreetingController.java
@@ -3,6 +3,8 @@ package com.pfy;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +21,8 @@ public class GreetingController {
 	@Autowired
 	EmployeeRepo repo;
 	
+	@Autowired
+	SessionFactory sessionFactory;
 	
     @RequestMapping("/greeting")
     public String greeting(@RequestParam("name") String name) {
@@ -31,17 +35,25 @@ public class GreetingController {
     {
     	ArrayList<EmployeeDetailss> list=new ArrayList<EmployeeDetailss>();
     	EmployeeDetailss ed=new EmployeeDetailss();
+    	Session session=sessionFactory.getCurrentSession();
+    	session.beginTransaction();
     	ed.setEmail("employee1@gmail.com");
     	ed.setName("Employee1");
     	ed.setId(1);
     	list.add(ed);
-    	repo.save(ed);
+    	session.save(ed);
+    	//repo.save(ed);
     	ed.setEmail("employee2@gmail.com");
     	ed.setName("Employee2");
     	ed.setId(2);
     	list.add(ed);
-    	repo.save(ed);
+    	session.save(ed);
+    	session.save(ed);
+    	List<EmployeeDetailss> list2=session.createQuery("from EmployeeDetailss").list();
+    	//repo.save(ed);
     	
-    	return repo.findAll();
+    	
+    	
+    	return list2;
     }
 }
